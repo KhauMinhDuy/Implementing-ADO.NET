9502bbc6a4addf7516f947d6141dca6c3acd2aee
khauminhduy <khauminhduy@gmail.com>
2022-09-17 18:08:32 +0700

    Commit

--- ADONET-Samples/ManagerClasses/ProductManager.cs
+++ ADONET-Samples/ManagerClasses/ProductManager.cs
@@ -3,37 +3,37 @@ using System.Data.SqlClient;
 
 namespace ADONET_Samples.ManagerClasses
 {
-  public class ProductManager
-  {
-    public const string PRODUCT_SQL = "SELECT ProductId, ProductName, IntroductionDate, Url, Price, RetireDate, ProductCategoryId FROM Product";
-    public const string PRODUCT_CATEGORY_SQL = "SELECT ProductCategoryId, CategoryName FROM ProductCategory";
-
-    #region GetProductsAsDataTable Method
-    public DataTable GetProductsAsDataTable()
+    public class ProductManager
     {
-      DataTable ret = null;
+        public const string PRODUCT_SQL = "SELECT ID, ProductName, IntroductionDate, Url, Price, RetireDate, ProductCategoryId FROM Product";
+        public const string PRODUCT_CATEGORY_SQL = "SELECT ID, CategoryName FROM ProductCategory";
 
-      // Create a connection
-      using (SqlConnection ConnectionObject = new SqlConnection(AppSettings.ConnectionString))
-      {
-        // Open the connection
-        ConnectionObject.Open();
-
-        // Create command object
-        using (SqlCommand CommandObject = new SqlCommand(PRODUCT_SQL, ConnectionObject))
+        #region GetProductsAsDataTable Method
+        public DataTable GetProductsAsDataTable()
         {
-          // Create a SQL Data Adapter
-          using (SqlDataAdapter da = new SqlDataAdapter(CommandObject))
-          {
-            // Fill DataTable using Data Adapter
-            ret = new DataTable();
-            da.Fill(ret);
-          }
-        }
-      }
+            DataTable ret = null;
 
-      return ret;
+            // Create a connection
+            using (SqlConnection ConnectionObject = new SqlConnection(AppSettings.ConnectionString))
+            {
+                // Open the connection
+                ConnectionObject.Open();
+
+                // Create command object
+                using (SqlCommand CommandObject = new SqlCommand(PRODUCT_SQL, ConnectionObject))
+                {
+                    // Create a SQL Data Adapter
+                    using (SqlDataAdapter da = new SqlDataAdapter(CommandObject))
+                    {
+                        // Fill DataTable using Data Adapter
+                        ret = new DataTable();
+                        da.Fill(ret);
+                    }
+                }
+            }
+
+            return ret;
+        }
+        #endregion
     }
-    #endregion
-  }
 }
