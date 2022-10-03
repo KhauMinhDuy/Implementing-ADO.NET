64c0d6344553f4e94d7364e0226620381fafaeee
khauminhduy <khauminhduy@gmail.com>
2022-10-03 15:11:38 +0700

    Init

---
+++ ADO.NET Tutorials/ADO.NET Tutorials/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.NET_Tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
